699f32466160b0f9b4a414d07b9124ae57ee631d
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2025-01-14 21:57:49 +0100

    static commit

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -121,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Directory where collected static files will be stored
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional static file locations (if needed)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
